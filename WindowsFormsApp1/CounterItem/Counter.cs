3519f0acec4aea6811746a2b0ea2190bd007031e
uiseong.chae <iop245@naver.com>
2022-12-13 17:16:28 +0900

    average 계산 코드 오류 수정

--- WindowsFormsApp1/CounterItem/Counter.cs
+++ WindowsFormsApp1/CounterItem/Counter.cs
@@ -56,7 +56,7 @@ namespace WindowsFormsApp1.CounterItem
         {
             //Average계산과 RecordCount 증가 순서 주의,
             //RcordCount 증가 전에 Average 계산 하도록 구현됨 
-            average = average * ((double)recordCount / recordCount + 1) + (double)value / recordCount + 1;
+            average = average * ((double)recordCount / (recordCount + 1)) + (double)value / (recordCount + 1);
             recordCount++;
 
             if (minValue > value)
